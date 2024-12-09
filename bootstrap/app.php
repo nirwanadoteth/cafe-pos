b948112a5d4b80bcc3f09f7baad50cae34c344b0
Irkham <irkham.dev@gmail.com>
2024-12-10 06:53:42 +0700

    Configure trusted proxies

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -13,7 +13,8 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware) {
-        $middleware->prepend(RenderTorchlight::class);
+        $middleware->prepend(RenderTorchlight::class)
+            ->trustProxies(at: '*');
     })
     ->withExceptions(function (Exceptions $exceptions) {
         Integration::handles($exceptions);
