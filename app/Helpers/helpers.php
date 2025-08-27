8101505f12a51499c115e21c8bd04609132fcb8c
irkham <121574317+10122222@users.noreply.github.com>
2025-08-28 02:11:10 +0700

    fix: update null coalescing operator to null coalescing assignment for better clarity in date parsing

--- app/Helpers/helpers.php
+++ app/Helpers/helpers.php
@@ -12,8 +12,8 @@ if (function_exists('getCarbonInstancesFromDateString') === false) {
 
         [$from, $to] = $dateString !== null ? explode(' - ', $dateString) : [now()->format($format), now()->format($format)];
 
-        $from = Carbon::createFromFormat($format, $from) ?? now();
-        $to = Carbon::createFromFormat($format, $to) ?? now();
+        $from = Carbon::createFromFormat($format, $from) ?: now();
+        $to = Carbon::createFromFormat($format, $to) ?: now();
 
         $diff = $from->diffInDays($to);
 
