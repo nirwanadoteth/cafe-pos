9c21f5dfb07e98d3a77547bdc24171855a8fac9a
irkham <121574317+10122222@users.noreply.github.com>
2025-08-30 22:25:54 +0700

    refactor(config): update config files for improved type safety and options
    Refactored config files to use explicit type casting, improved option naming, and added new configuration options for Redis, mail, filesystems, and session. Enhanced maintainability and reliability.

--- config/logging.php
+++ config/logging.php
@@ -54,12 +54,8 @@ return [
 
         'stack' => [
             'driver' => 'stack',
-            'channels' => explode(',', env('LOG_STACK', 'single')),
+            'channels' => explode(',', (string) env('LOG_STACK', 'single')),
             'ignore_exceptions' => false,
-        ],
-
-        'flare' => [
-            'driver' => 'flare',
         ],
 
         'single' => [
@@ -102,10 +98,10 @@ return [
             'driver' => 'monolog',
             'level' => env('LOG_LEVEL', 'debug'),
             'handler' => StreamHandler::class,
-            'formatter' => env('LOG_STDERR_FORMATTER'),
-            'with' => [
+            'handler_with' => [
                 'stream' => 'php://stderr',
             ],
+            'formatter' => env('LOG_STDERR_FORMATTER'),
             'processors' => [PsrLogMessageProcessor::class],
         ],
 
